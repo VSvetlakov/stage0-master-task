86efb3d9cec1c2ae440572da600995515917edec
VSvetlakov <116279684+VSvetlakov@users.noreply.github.com>
2023-11-18 15:24:19 +0400

    v. 1

--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -1,13 +1,11 @@
 package com.epam.loops;
 
-import java.util.Scanner;
-
 public class MultiplicationTable {
 
     public void printTable(int numberTableToPrint) {
 
         for (int i = 1; i <= 10; i++) {
-            System.out.println(String.format("%s x %s = %s",i,numberTableToPrint,i*numberTableToPrint));
+            System.out.println("%s x %s = %s".formatted(i, numberTableToPrint, i*numberTableToPrint));
         }
     }
 
